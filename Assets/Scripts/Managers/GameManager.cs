72c9f2a620d0477a9f428f4bcbdcde59901776d6
Excell Pepple <49886472+excellpepple@users.noreply.github.com>
2023-02-03 20:25:01 -0500

    Game State Initial Setup
    Game Manger
    Resource Manager

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public bool isGameRunning;
+
+    public bool isLevelActive;
+    
+    public bool isGameOver;
+
+    public GameObject playerCharacter;
+
+    public GameObject rootManager;
+    
+    public List<GameObject> mobsEnemies = new List<GameObject>();
+    
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        isGameRunning = true;
+        isGameOver = false;
+        isLevelActive = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
