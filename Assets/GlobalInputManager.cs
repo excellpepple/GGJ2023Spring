79641130d8d23f67986fa0bb1404f013a09d6f2c
Romano McBride <romano.mcbride@unity3d.com>
2023-02-05 14:41:52 -0500

    ability to jump into root

--- Assets/GlobalInputManager.cs
+++ Assets/GlobalInputManager.cs
@@ -6,30 +6,53 @@ using UnityEngine;
 public class GlobalInputManager : MonoBehaviour
 {
     public KeyCode KeySwitchCharacterRoots = KeyCode.Tab;
+    /// <summary>
+    /// modes:
+    /// true:   controlling roots
+    /// false:  controlling character
+    /// </summary>
     public bool mode;
+
+    public RootManager rm;
 
     public GameObject character;
     //public GameObject
     // Start is called before the first frame update
     void Start()
     {
-        
+        rm = FindObjectOfType<RootManager>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (mode)
+        if (mode) //roots
         {
-
+            if (Input.GetKeyDown(KeySwitchCharacterRoots))
+            {
+                mode = !mode;
+                character.SetActive(true);
+                character.transform.position = rm.currentPoint.position;
+            }
+            
+            if (Input.GetKey(KeyCode.UpArrow))
+            {
+                rm.Traverse(-1);
+            }
+            if (Input.GetKey(KeyCode.DownArrow))
+            {
+                rm.Traverse(1);
+            }
         }
-        else
+        else //character
         {
+            if (Input.GetKeyDown(KeySwitchCharacterRoots))
+            {
+                character.SetActive(false);
+                mode = !mode;
+            }
         }
         
-        if (Input.GetKeyDown(KeySwitchCharacterRoots))
-        {
-            mode = !mode;
-        }
+
     }
 }
